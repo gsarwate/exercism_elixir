132fe1aacbec0e717e3060d3b655b520e24dc183
gsarwate <4589380+gsarwate@users.noreply.github.com>
2019-11-10 10:33:12 -0800

    Re-arrange the @doc comment for a function

--- rna-transcription/lib/rna_transcription.ex
+++ rna-transcription/lib/rna_transcription.ex
@@ -1,4 +1,11 @@
 defmodule RnaTranscription do
+  @transcription %{
+    ?G => ?C,
+    ?C => ?G,
+    ?T => ?A,
+    ?A => ?U
+  }
+
   @doc """
   Transcribes a character dna representing DNA nucleotides to RNA
 
@@ -7,14 +14,6 @@ defmodule RnaTranscription do
   iex> RnaTranscription.to_rna('ACTG')
   'UGAC'
   """
-
-  @transcription %{
-    ?G => ?C,
-    ?C => ?G,
-    ?T => ?A,
-    ?A => ?U
-  }
-
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
     Enum.map(dna, fn c -> @transcription[c] end)
